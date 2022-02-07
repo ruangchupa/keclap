3e282cc99938f141b816452d7204f565899f0a8b
Muhammad Yusuf <muhammad.yusuf@live.com>
2022-02-07 19:22:42 +0700

    Ngaleresken judul dina AboutModal

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Keclap" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         Diciptakeun di Padalarang - Bandung Barat. Nyandak tina proyek {' '}
         <a
